fddc65454895c0e69f6719e781a85508566170fc
tungakanui <tung.ngthanh99@gmail.com>
2024-08-06 14:33:32 +0700

    Initial Commit

---
+++ event_countdown/event_countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  event_countdownApp.swift
+//  event_countdown
+//
+//  Created by Tnui on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct event_countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
